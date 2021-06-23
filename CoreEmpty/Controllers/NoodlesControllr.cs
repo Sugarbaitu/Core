e933447b81cbb7c896641466cd6cacac0d47b78d
chen <chenyaofeng@>
2021-06-23 17:37:40 +0800

    1

---
+++ CoreEmpty/Controllers/NoodlesControllr.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreEmpty.Controllers
+{
+    [Route("admin/[controller]/[action]")]
+    public class NoodlesController : Controller
+    {
+        [HttpPost]
+        public IList<string> Index(int id,IFormCollection formCollection)
+        {
+            return new List<string> { id + "", "面条", "饼", "豆" };
+        }
+    }
+}
